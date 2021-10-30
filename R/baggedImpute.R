3869045a148fcfe317efe75294ec7b49bedd01dd
NipoDoge <70971096+NipoDoge@users.noreply.github.com>
2021-10-29 22:59:17 -0300

    Update baggedImpute.R

--- R/baggedImpute.R
+++ R/baggedImpute.R
@@ -1,10 +1,10 @@
 #' @import stats
-#' @importFrom imputeTS na.interpolation
-#' @importFrom imputeTS na.kalman
-#' @importFrom imputeTS na.ma
+#' @importFrom imputeTS na_interpolation
+#' @importFrom imputeTS na_kalman
+#' @importFrom imputeTS na_ma
 #' @export
 
-baggedImpute <- function(data, bootrep = 1000, blocklen = 2, BBM = c("MBB", "NBB", "CBB"), misstr = c("ams", "ivs"), interpolation = c("Stineman", "Kalman", "WMA") ){
+baggedImpute <- function(data, bootrep = 1000, blocklen = 2, BBM = c("MBB", "NBB", "CBB"), misstr = c("ams", "ivs"), interpolation = c("Stineman", "Kalman", "SMA", "LWMA", "EWMA", "Linear"), WMAk = 2){
   if (BBM == "CBB"){
     BootFunc <- c(GenCBB)  
   } else if (BBM == "MBB"){
@@ -18,13 +18,28 @@ baggedImpute <- function(data, bootrep = 1000, blocklen = 2, BBM = c("MBB", "NBB
     
     boot.result <- BootFunc[[1]](Xt = data, l = blocklen, r = bootrep, irreg = misstr) 
     if (interpolation == "Stineman"){
-      z <- na.interpolation(boot.result, option = "stine")
+      z <- na_interpolation(boot.result, option = "stine")
       RegularSeries <- apply(z , 1, mean , na.rm = TRUE)
     } else if (interpolation == "Kalman"){
-      z <- na.kalman(x = boot.result, model = "auto.arima", smooth = TRUE)
+      z <- na_kalman(x = boot.result, model = "auto.arima", smooth = TRUE)
       RegularSeries <-apply(z , 1, mean , na.rm = TRUE)
-    } else if (interpolation == "WMA"){
-      z <- na.ma(x = boot.result, k = 4, weighting = "exponential")
+    } else if (interpolation == "SMA"){
+      if(WMAk > 0){
+        z <- na_ma(x = boot.result, k = WMAk, weighting = "simple")
+        RegularSeries <-apply(z , 1, mean , na.rm = TRUE)
+        }
+    } else if (interpolation == "LWMA"){
+      if(WMAk > 0){
+        z <- na_ma(x = boot.result, k = WMAk, weighting = "linear")
+        RegularSeries <-apply(z , 1, mean , na.rm = TRUE)
+        }
+    } else if (interpolation == "EWMA"){
+      if(WMAk > 0){
+        z <- na_ma(x = boot.result, k = WMAk, weighting = "exponential")
+        RegularSeries <-apply(z , 1, mean , na.rm = TRUE)
+        }
+    } else if (interpolation == "Linear"){
+      z <- na_interpolation(x = boot.result, option = "linear")
       RegularSeries <-apply(z , 1, mean , na.rm = TRUE)
     }
   }else{    #combine with ivs
@@ -36,9 +51,24 @@ baggedImpute <- function(data, bootrep = 1000, blocklen = 2, BBM = c("MBB", "NBB
     } else if (interpolation == "Kalman"){
       z <- na.kalman(x=boot.result, model = "auto.arima", smooth = TRUE)
       RegularSeries <- apply(z , 1, mean , na.rm = TRUE)
-    } else if (interpolation == "WMA"){ 
-      z <- na.ma(x=boot.result, k = 4, weighting = "exponential")
-      RegularSeries <- apply(z , 1, mean , na.rm = TRUE)
+    } else if (interpolation == "SMA"){
+      if(WMAk > 0){
+        z <- na_ma(x = boot.result, k = WMAk, weighting = "simple")
+        RegularSeries <-apply(z , 1, mean , na.rm = TRUE)
+        }
+    } else if (interpolation == "LWMA"){
+      if(WMAk > 0){
+        z <- na_ma(x = boot.result, k = WMAk, weighting = "linear")
+        RegularSeries <-apply(z , 1, mean , na.rm = TRUE)
+        }
+    } else if (interpolation == "EWMA"){
+      if(WMAk > 0){
+        z <- na_ma(x = boot.result, k = WMAk, weighting = "exponential")
+        RegularSeries <-apply(z , 1, mean , na.rm = TRUE)
+        }
+    } else if (interpolation == "Linear"){
+      z <- na_interpolation(x = boot.result, option = "linear")
+      RegularSeries <-apply(z , 1, mean , na.rm = TRUE)
     }
   }
   imputedSeries[listNA] <- RegularSeries[listNA]
